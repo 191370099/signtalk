c753c08b3ff842fba313495265d8cc082b810c2e
Salman Ahmad <191370099@gift.edu.pk>
2023-07-12 15:39:32 +0500

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
 import ThreeScene from './ThreeScene';
+import { useState } from 'react';
 
 function App() {
+
+  const [inputText, setInputText] = useState('');
+  const [submittedText, setSubmittedText] = useState('');
   
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmittedText(inputText);
+  };
   return (
     <div>
-      {/* <h1>My React Three.js App</h1> */}
-      <ThreeScene />
+      {/* <div> */}
+        <ThreeScene text={submittedText}/>
+      {/* </div> */}
+      <form onSubmit={handleSubmit}>
+        <input value={inputText} onChange={handleInputChange} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
--- src/ThreeScene.jsx
+++ src/ThreeScene.jsx
@@ -5,7 +5,7 @@ import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm
 import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
 
-const ThreeScene = () => {
+const ThreeScene = (props) => {
     useEffect(() => {
     let scene, camera, renderer, mixer;
     scene = new THREE.Scene();
@@ -16,7 +16,7 @@ const ThreeScene = () => {
       1000
     );
     camera.position.z = 2;
-    camera.position.y = 2.5;
+    camera.position.y = 1.5;
     const canvas = document.getElementById('myThreeJsCanvas');
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     ambientLight.castShadow = true;
@@ -24,31 +24,57 @@ const ThreeScene = () => {
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(1, 1, 1).normalize();
     scene.add(directionalLight);
+    const textArray = props.text.split(' ');
+    // console.log(textArray[0]);
+    // const loader = new FBXLoader();
     const loader = new GLTFLoader();
     loader.setPath("/resources/zombie/");
     loader.load("first.glb", (fbx) => {
-        console.log(fbx.animations);
+        // console.log(fbx.animations);
         scene.add(fbx.scene);
-        fbx.scene.scale.set(1.7,1.7,1.7);
-        mixer = new THREE.AnimationMixer(fbx.scene);
-        const howclip = THREE.AnimationClip.findByName(fbx.animations,'how')
-        const howaction = mixer.clipAction(howclip);
-        howaction.play();
-        howaction.loop = THREE.LoopOnce;
-        const youclip = THREE.AnimationClip.findByName(fbx.animations,'you')
-        const youaction = mixer.clipAction(youclip);
-        mixer.addEventListener('finished', function(e){
-          if(e.action._clip.name== 'how'){
-            youaction.crossFadeFrom(howaction,0.5,true);
-            youaction.play();
-            youaction.loop = THREE.LoopOnce;
-          }
-        });
+        // fbx.scale.set(0.009,0.009,0.009);
+        // console.log(textArray.length);
+        const animationArray = new Array(textArray.length).fill(undefined);
+        if(textArray.length>1){
+          mixer = new THREE.AnimationMixer(fbx.scene);
+          let i=0, clip;
+          textArray.forEach(word => {
+            clip = THREE.AnimationClip.findByName(fbx.animations,word);
+            animationArray[i++] = mixer.clipAction(clip);
+          });
+          console.log(mixer);
+          let j=0;
+          animationArray[j].play();
+          animationArray[j].loop = THREE.LoopOnce;
+          mixer.addEventListener('finished', function(e){
+            // console.log(e.action._clip.name,animationArray[j]);
+              if(j<animationArray.length-1){
+                console.log("Salman"+animationArray.length);
+                animationArray[++j].crossFadeFrom(animationArray[j-1],0.5,true);
+                animationArray[j].play();
+                animationArray[j].loop = THREE.LoopOnce;
+              }
+          });
+        }
+        // const howclip = THREE.AnimationClip.findByName(fbx.animations,'padel')
+        // const howaction = mixer.clipAction(howclip);
+        // howaction.fadeIn(0.4);
+        // howaction.play();
+        // howaction.loop = THREE.LoopOnce;
+        // const youclip = THREE.AnimationClip.findByName(fbx.animations,'you')
+        // const youaction = mixer.clipAction(youclip);
+        // mixer.addEventListener('finished', function(e){
+        //   if(e.action._clip.name== 'how'){
+        //     youaction.crossFadeFrom(howaction,0.5,true);
+        //     youaction.play();
+        //     youaction.loop = THREE.LoopOnce;
+        //   }
+        // });
         
     });
     renderer = new THREE.WebGLRenderer({canvas, antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xcbf2db);
+    renderer.setClearColor(0xffffff);
     document.body.appendChild(renderer.domElement);
     const clock = new THREE.Clock();
     function animate() {
@@ -58,7 +84,8 @@ const ThreeScene = () => {
       renderer.render(scene,camera);
     }
     renderer.setAnimationLoop(animate);
-  }, []);
+  }, [props.text]);
+  console.log(props.text);
   return (
     <div>
         <canvas id='myThreeJsCanvas'/>
